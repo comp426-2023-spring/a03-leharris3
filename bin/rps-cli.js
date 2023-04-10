5c28498dbec8803df728ea327ae096861207eeb9
leharris3 <levlevi@email.unc.edu>
2023-04-10 13:48:24 -0400

    import rps

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import minimist from 'minimist';
-import { rpsls } from "../lib/rpsls.js";
+import { rps } from "../lib/rpsls.js";
 const args = process.argv.slice(2);
 rpsls(args);
